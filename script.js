871cc1d0a580c4238b092f4de40a7dd872b191f1
origin <ST10447186@vcconnect.edu.za>
2024-09-12 19:13:35 +0200

    feature signup working

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Perform validation or login process here
+    alert("Login successful!");
+});
